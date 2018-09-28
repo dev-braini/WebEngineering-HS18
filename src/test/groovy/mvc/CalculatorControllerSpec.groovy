d11f25c8ace84729bcd12ce57fd3878cfca22688
ribeaud <christian.ribeaud@fhnw.ch>
2018-09-28 18:46:22 +0200

    Fix 'CalculatorControllerSpec' as it is using deprecated (and absent) code

--- src/test/groovy/mvc/CalculatorControllerSpec.groovy
+++ src/test/groovy/mvc/CalculatorControllerSpec.groovy
@@ -1,35 +1,24 @@
 package mvc
 
-// import grails.test.mixin.TestFor
+
+import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
-import spock.lang.Unroll
 
 /**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ * See the API for {@link ControllerUnitTest} for usage instructions.
  */
-// @TestFor(CalculatorController)
-class CalculatorControllerSpec extends Specification {
+class CalculatorControllerSpec extends Specification implements ControllerUnitTest<CalculatorController> {
 
-    CalculatorController calculatorController
-
-    def setup() {
-        calculatorController = new CalculatorController()
-    }
-
-    def cleanup() {
-    }
-
-    @Unroll
     void "average of #en and #exam should be #result"(en, exam, result) {
         when:
-            calculatorController.calc(en, exam)
+        controller.calc(en, exam)
         then: "average calculation"
-            model.result == result
+        model.result == result
         where:
-            en  | exam | result
-            0.0 | 0.0  | 0.0
-            1.0 | 2.0  | 1.5
-            2.0 | 1.0  | 1.5
+        en  | exam | result
+        0.0 | 0.0  | 0.0
+        1.0 | 2.0  | 1.5
+        2.0 | 1.0  | 1.5
 
     }
 }
